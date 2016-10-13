f912a4321c9df80cb81cba52f5601fc425a09c84
Dhaval Sharma <me@dhavalsharma.com>
2016-10-13 20:21:50 +0530

    added demo image

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-A simple membership extension for civicrm. It will list the existing memberships and you can filter the memberships based on from/to dates.
+
+## CiviCRM Memberships listings extension
+
+A simple membership extension for civicrm. 
+It will list the existing memberships and you can filter the memberships based on from/to dates.
+
+The URL for the debug version will look like 
+*http://localhost/~dhaval/drupal-7.50/civicrm/a/#/listmemberships?angularDebug=1*
+
+and without debug info will be 
+*http://localhost/~dhaval/drupal-7.50/civicrm/a/#/listmemberships*
+
+The extension will be located in directly similar to 
+*/drupal-7.50/sites/default/files/civicrm/ext/com.dhaval.memberships*
+
+This is how the screen would look for the extension
+![Screenshot of Membership List][demo2]
+
+[demo2]: https://github.com/dhavalsharma/civicrm-memberships/tree/master/demo/civicrm-membership-screen.png "Screenshot Membership List"
--- ang/memberships/ListMembershipsCtrl.js
+++ ang/memberships/ListMembershipsCtrl.js
@@ -8,12 +8,6 @@
         // If you need to look up data when opening the page, list it out
         // under "resolve".
         resolve: {
-          myContact: function(crmApi) {
-            return crmApi('Contact', 'getsingle', {
-              id: 'user_contact_id',
-              return: ['first_name', 'last_name']
-            });
-          },
           allMemberships: function(crmApi){
             return crmApi('Membership', 'get', {
                   sequential: 1
@@ -27,7 +21,7 @@
   // The controller uses *injection*. This default injects a few things:
   //   $scope -- This is the set of variables shared between JS and HTML.
   //   crmApi, crmStatus, crmUiHelp -- These are services provided by civicrm-core.
-  //   myContact -- The current contact, defined above in config().
+
   angular.module('memberships').controller('MembershipsListMembershipsCtrl', 
     function($scope, crmApi, crmStatus, crmUiHelp, allMemberships) {
     // The ts() and hs() functions help load strings for this module.
@@ -39,64 +33,6 @@
     $scope.allMemberships = allMemberships;
     //filter dates
     $scope.filterdates = {};
-/*
-    // We have myContact available in JS. We also want to reference it in HTML.
-    $scope.myContact = myContact;
-    $scope.save = function save() {
-      return crmStatus(
-        // Status messages. For defaults, just use "{}"
-        {start: ts('Saving...'), success: ts('Saved')},
-        // The save action. Note that crmApi() returns a promise.
-        crmApi('Contact', 'create', {
-          id: myContact.id,
-          first_name: myContact.first_name,
-          last_name: myContact.last_name
-        })
-      );
-    };
-
-    $scope.filterDateRange = function(filterdates){
-      return function(value){
-        return compareDate(value, filterdates.start, filterdates.end);
-        function compareDate(value, start, end){
-          console.log(value, start, end);
-          if(!start && !end){
-            return true;
-          }
-          var valuestartts, valueendts,startts,endts;
-          
-          valuestartts = new Date(value.start_date);
-          valuestartts = valuestartts.getTime();
-          if(value.end_date){
-            valueendts = new Date(value.end_date);
-            valueendts = valueendts.getTime();           
-          }
-          else{
-            valueendts = Infinity;//for lifetime
-          }
-          if(start){
-            startts = new Date(start);
-            startts = startts.getTime();
-          }
-          else{
-            startts = -Infinity;
-          }
-          if(end){
-            endts = new Date(end);
-            endts = endts.getTime();
-          }
-          else{
-            endts = Infinity;
-          }
-
-          if(valuestartts >= startts && valueendts <= endts){
-            return true;
-          }
-          return false;
-        }
-      }
-    }
-*/
   });
 
   //filter to check if dates in array are in range
